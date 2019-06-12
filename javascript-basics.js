cd72e9bf113d2ac49dac5db19bd4ff7303e7d0a1
Samantha Jeet <samanthajeet@gmail.com>
2019-06-12 11:13:06 -0700

    variable and data types

---
+++ javascript-basics.js
@@ -0,0 +1,22 @@
+/****************************
+ * Variable and data types
+ */
+
+let firstName = 'Sam';
+console.log(firstName);
+
+let lastName = 'Jeet';
+let age = 30;
+
+let fullAge = true;
+console.log(fullAge);
+
+let job;
+console.log(job);
+
+job = 'web developer'
+console.log(job);
+
+/****************************
+ * Variable mutation and type coercion
+ */
